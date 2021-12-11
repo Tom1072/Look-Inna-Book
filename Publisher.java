70c054ce6a66978651628910cfdd266d34a6dda4
Tom1072 <lamvutungvt@gmail.com>
2021-12-11 18:05:07 -0500

    Table initialized with JDBC connection established

--- Publisher.java
+++ Publisher.java
@@ -3,9 +3,9 @@ public class Publisher {
     public String email;
     public String bank_account;
     public String address;
-    public long phone_number;
+    public String phone_number;
 
-    public Publisher(int publisher_id, String email, String bank_account, String address, long phone_number) {
+    public Publisher(int publisher_id, String email, String bank_account, String address, String phone_number) {
         this.publisher_id = publisher_id;
         this.email = email;
         this.bank_account = bank_account;
